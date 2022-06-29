bd5bcf2760542dff05722eb15110e3879f570405
Arafat <arafat97alim@gmail.com>
2022-06-29 14:56:57 +0530

    Parent and child components.

--- Page.js
+++ Page.js
@@ -1,23 +1,46 @@
 //! creating a custom react component
 
+//! Header component (Child component)
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.png" alt="react-logo" width="45px" />
+      </nav>
+    </header>
+  );
+}
+
+//! Main component (Child component)
+function MainComponent() {
+  return (
+    <>
+      <h1>I love React </h1>
+      <ol>
+        <li>I want to Learn React, becuase i love JS</li>
+        <li>React makes me feel more comfortable to code</li>
+        <li>WE devloper have a huge community on React</li>
+        <li>React gives our code more reusuablity, extensibility</li>
+      </ol>
+    </>
+  );
+}
+//! Footer component (Child component)
+function Footer() {
+  return (
+    <footer>
+      <p>© 2022 Alim development. All rights reserved.</p>
+    </footer>
+  );
+}
+
+//! Page compoenent (Parent component)
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" alt="react-logo" width="45px" />
-          <h1>I love React </h1>
-          <ol>
-            <li>I want to Learn React, becuase i love JS</li>
-            <li>React makes me feel more comfortable to code</li>
-            <li>WE devloper have a huge community on React</li>
-            <li>React gives our code more reusuablity, extensibility</li>
-          </ol>
-        </nav>
-      </header>
-      <footer>
-        <p>© 2022 Alim development. All rights reserved.</p>
-      </footer>
+      <Header />
+      <MainComponent />
+      <Footer />
     </div>
   );
 }
