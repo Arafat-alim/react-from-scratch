2da77db677471d3319271bc49611a64bf4d83468
Arafat <arafat97alim@gmail.com>
2022-06-29 21:19:14 +0530

    styling nabar

--- react-official-fundamentals/src/components/Navbar.js
+++ react-official-fundamentals/src/components/Navbar.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Navbar() {
   return (
-    <header>
-      <nav>
-        <img
-          src="../images/react-logo.png"
-          alt="react-logo"
-          className="nav--logo"
-        />
-      </nav>
-    </header>
+    <nav>
+      <img
+        src="../images/react-logo.png"
+        alt="react-logo"
+        className="nav--icon"
+      />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--title">React Course - 1</h4>
+    </nav>
   );
 }
 
