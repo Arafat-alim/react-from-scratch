a452041b5a1653c0ae4ca4960488af9b814046b1
Arafat <arafat97alim@gmail.com>
2022-06-29 14:01:59 +0530

    static page added.

---
+++ static-project.js
@@ -0,0 +1,17 @@
+const staticPage = (
+  <div>
+    <nav>
+      <img src="./react-logo.png" alt="react-logo" width="40px" />
+      <h1>Fun Fact About React</h1>
+      <ul>
+        <li>Was First Release in 2013</li>
+        <li>Was orginally created by JORDAN WALKE</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is Maintained by Facebook</li>
+        <li>Powers thoursands of enterprise apps, including mobile apps</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+ReactDOM.render(staticPage, document.getElementById("root"));
