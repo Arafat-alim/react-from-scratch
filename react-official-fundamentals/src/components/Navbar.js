06ae3802eba989803e99e826ed29e9fbdeee2605
Arafat <arafat97alim@gmail.com>
2022-06-29 22:42:13 +0530

    logo is now showing

--- react-official-fundamentals/src/components/Navbar.js
+++ react-official-fundamentals/src/components/Navbar.js
@@ -1,13 +1,10 @@
 import React from "react";
+import logo from "../images/react-logo.png";
 
 function Navbar() {
   return (
     <nav>
-      <img
-        src="../images/react-logo.png"
-        alt="react-logo"
-        className="nav--icon"
-      />
+      <img src={logo} alt="react-logo" className="nav--icon" />
       <h3 className="nav--logo_text">React Facts</h3>
       <h4 className="nav--title">React Course - 1</h4>
     </nav>
