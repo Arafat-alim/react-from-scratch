c59760f6a0bfb550c84d1253441b7f2f6ed06021
Arafat <arafat97alim@gmail.com>
2022-07-01 00:17:16 +0530

    timeofday rendering

--- react-official-fundamentals/src/components/PropComponent.jsx
+++ react-official-fundamentals/src/components/PropComponent.jsx
@@ -2,13 +2,27 @@ import React from "react";
 
 export default function PropComponent() {
   // Dynamic Rendering
-  const firstName = "Arafat";
-  const lastName = "Alim";
+  //   const firstName = "Arafat";
+  //   const lastName = "Alim";
+
+  //! CHallenge timeOfDAY
+  let timeOfDay;
+  let message;
+  timeOfDay = new Date().getHours();
+
+  if (timeOfDay < 12) {
+    message = "Morning";
+  } else if (timeOfDay > 12 && timeOfDay <= 17) {
+    message = "Evening";
+  } else {
+    message = "Night";
+  }
+
+  let date = new Date();
   return (
     <div>
-      <h1>
-        Hello! {firstName} {lastName}
-      </h1>
+      <h1>It is about {date.getHours()}</h1>
+      <p>Good {message}</p>
     </div>
   );
 }
