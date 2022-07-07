cb191928e79713b5d47f2b8e8265031a879d7c55
arafat-alim <arafat97alim@gmail.com>
2022-07-07 18:32:56 +0530

    managed object using useState.

--- react-official-fundamentals/src/components/ComplexStateObject.jsx
+++ react-official-fundamentals/src/components/ComplexStateObject.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import user from "../images/user.png";
 import starEmpty from "../images/star-empty.png";
-// import starFilled from "../images/star-filled.png";
+import starFilled from "../images/star-filled.png";
 
 export default function ComplexStateObject() {
+  //! creating state
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+
+  function toggleFavorite() {
+    setContact((prevState) => {
+      return {
+        ...prevState,
+        isFavorite: !prevState.isFavorite,
+      };
+    });
+  }
   return (
     <main>
       <article className="card">
         <img src={user} alt="images" className="card--logo" />
         <div className="card--info">
-          <img src={starEmpty} alt="" className="card--favourite" />
-          <h2 className="card--name">Joe</h2>
-          <p className="card--contact">+1 (719) 555-1212</p>
-          <p className="card--contact">itsmyrealname@example.com</p>
+          <img
+            src={contact.isFavorite ? starFilled : starEmpty}
+            alt="fav-logo"
+            className="card--favourite"
+            onClick={toggleFavorite}
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
         </div>
       </article>
     </main>
   );
 }
-
-//  <main>
-//    <article className="card">
-//      <img src="" alt="" className="card--logo" />
-//      <div className="card--info">
-//        <img src="" alt=""  />
-
-//      </div>
-//    </article>
-//  </main>;
