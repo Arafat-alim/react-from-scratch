84a860d131a12e761934e690bedd42a8cdeeeeb2
arafat-alim <arafat97alim@gmail.com>
2022-07-07 13:43:56 +0530

    basic state added using useState.

--- react-official-fundamentals/src/components/ReactState.jsx
+++ react-official-fundamentals/src/components/ReactState.jsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 
 export default function ReactState() {
   const [data, setData] = useState("Yes");
+  function handleClick() {
+    setData("No");
+  }
   return (
     <div className="state">
       <h1 className="state--title">Is state important to know?</h1>
-      <div className="state--value">
+      <div className="state--value" onClick={handleClick}>
         <h1>{data}</h1>
       </div>
     </div>
