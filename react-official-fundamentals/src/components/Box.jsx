cea6e09624e8fd4d92cb6d85f4aeaf732944f709
arafat-alim <arafat97alim@gmail.com>
2022-07-08 22:57:18 +0530

    some minor changes on colors

--- react-official-fundamentals/src/components/Box.jsx
+++ react-official-fundamentals/src/components/Box.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Box(props) {
   const styles = {
-    backgroundColor: props.on === true ? "red" : "blue",
+    backgroundColor: props.on ? "#222222" : "transparent",
   };
   return (
     <div style={styles} className="box--element">
