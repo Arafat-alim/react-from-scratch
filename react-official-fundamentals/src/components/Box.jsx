cef55a401c1f574b06643ac2e719ef5cf1329e67
arafat-alim <arafat97alim@gmail.com>
2022-07-09 00:03:16 +0530

    imperative way to update the state.

--- react-official-fundamentals/src/components/Box.jsx
+++ react-official-fundamentals/src/components/Box.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 
 export default function Box(props) {
-  const [on, setOn] = useState(props.on);
+  //! derived state
+  //   const [on, setOn] = useState(props.on);
 
-  function toggleBtn() {
-    setOn((prevState) => !prevState);
-  }
+  //   function toggleBtn() {
+  //     setOn((prevState) => !prevState);
+  //   }
   const styles = {
-    backgroundColor: on ? "#222222" : "transparent",
+    backgroundColor: props.on ? "#222222" : "transparent",
   };
   return (
-    <div style={styles} className="box--element" onClick={toggleBtn}>
+    <div
+      style={styles}
+      className="box--element"
+      onClick={() => props.toggleBtn(props.id)}
+    >
       Box
     </div>
   );
--- react-official-fundamentals/src/components/BoxParent.jsx
+++ react-official-fundamentals/src/components/BoxParent.jsx
@@ -7,9 +7,9 @@ export default function BoxParent(props) {
   //! mapping for rendering
 
   // Dyanmic Style
-  const styles = {
-    backgroundColor: props.darkMode ? "#ccc" : "#000",
-  };
+  //   const styles = {
+  //     backgroundColor: props.darkMode ? "#ccc" : "#000",
+  //   };
 
   //   const boxes = box.map((item) => (
   //     <div style={styles} className="box--element" key={item.id}>
@@ -17,7 +17,29 @@ export default function BoxParent(props) {
   //     </div>
   //   ));
 
-  const boxElemeent = box.map((item) => <Box on={item.on} key={item.id} />);
+  //! Function toggle
+  function toggleBtn(id) {
+    setBox((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  }
+
+  const boxElemeent = box.map((item) => (
+    <Box on={item.on} key={item.id} toggleBtn={toggleBtn} id={item.id} />
+  ));
   return (
     <div className="box">
       <h1>Boxes will go here</h1>
