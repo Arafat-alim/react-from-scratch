daaff22f56334ce6bd4cd7c2a55ab45a6319e015
arafat-alim <arafat97alim@gmail.com>
2022-07-08 23:34:29 +0530

    local state.

--- react-official-fundamentals/src/components/Box.jsx
+++ react-official-fundamentals/src/components/Box.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Box(props) {
+  const [on, setOn] = useState(props.on);
+
+  function toggleBtn() {
+    setOn((prevState) => !prevState);
+  }
   const styles = {
-    backgroundColor: props.on ? "#222222" : "transparent",
+    backgroundColor: on ? "#222222" : "transparent",
   };
   return (
-    <div style={styles} className="box--element">
+    <div style={styles} className="box--element" onClick={toggleBtn}>
       Box
     </div>
   );
