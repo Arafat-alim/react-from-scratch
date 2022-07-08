58a48b19ef35dcc3d95edfdd7857611a440a2e75
arafat-alim <arafat97alim@gmail.com>
2022-07-08 20:56:24 +0530

    passing state as a prop

--- react-official-fundamentals/src/App.js
+++ react-official-fundamentals/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import ComplexStateObject from "./components/ComplexStateObject";
+import { useState } from "react";
+// import ComplexStateObject from "./components/ComplexStateObject";
+import Count from "./components/Count";
 
 function App() {
+  //! creating a state for count
+  const [count, setCount] = useState(0);
+
+  //creating an handler
+  function addCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
+  function subCount() {
+    setCount((prevCount) => prevCount - 1);
+  }
   return (
     <div className="container">
-      <ComplexStateObject />
+      <button onClick={addCount}>+</button>
+      <Count count={count} />
+      <button onClick={subCount}>-</button>
     </div>
   );
 }
