0050ab1526daaaa73beaa881df3f0099eacc3fe3
arafat-alim <arafat97alim@gmail.com>
2022-07-09 16:06:54 +0530

    Lengthy process to update the form fields. not recommedation.

--- react-official-fundamentals/src/components/Forms/Forms.jsx
+++ react-official-fundamentals/src/components/Forms/Forms.jsx
@@ -2,17 +2,24 @@ import React, { useState } from "react";
 
 export default function Forms() {
   //! creatin state
-  const [firstName, setFirstName] = React.useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   console.log(firstName);
+  console.log(lastName);
 
   function handleChange(event) {
     setFirstName(event.target.value);
+  }
+  //! handling latt name
+  function handleLast(event) {
+    setLastName(event.target.value);
   }
 
   return (
     <form>
       <input type="text" placeholder="First Name" onChange={handleChange} />
+      <input type="text" placeholder="Last Name" onChange={handleLast} />
     </form>
   );
 }
