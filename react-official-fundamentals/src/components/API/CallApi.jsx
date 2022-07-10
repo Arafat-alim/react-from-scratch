2cbe75a78180effd2648bcd0aae1a2c8eb801e17
arafat-alim <arafat97alim@gmail.com>
2022-07-10 15:59:09 +0530

    added next character button fetching data from the api.

--- react-official-fundamentals/src/components/API/CallApi.jsx
+++ react-official-fundamentals/src/components/API/CallApi.jsx
@@ -4,20 +4,32 @@ import { useState, useEffect } from "react";
 export default function CallApi() {
   //! saving api calls into the state
   const [starWarsData, setStarWarsData] = useState({});
-
+  const [num, setNum] = useState(1);
   console.log("Component Rendering");
+
+  //! Handle next character
+  function nextChar() {
+    setNum((prevNum) => {
+      return prevNum + 1;
+    });
+  }
 
   //! API's calling
   // side Effect
-  useEffect(function () {
-    console.log("Ran Effect");
-    fetch("https://swapi.dev/api/people/1")
-      .then((res) => res.json())
-      .then((data) => setStarWarsData(data));
-  }, []);
+  useEffect(
+    function () {
+      console.log("Ran Effect");
+      fetch(`https://swapi.dev/api/people/${num}`)
+        .then((res) => res.json())
+        .then((data) => setStarWarsData(data));
+    },
+    [num]
+  );
   return (
     <div>
       <h1>APi rendering</h1>
+      <h1>The count is {num}</h1>
+      <button onClick={nextChar}>Next Character</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
